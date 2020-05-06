9a3d480a5b81a9b55313d77a65c65b080e5e2a89
David Hirst <dhirst@ametroslearning.com>
2020-05-05 20:42:59 -0400

    finishes authorization method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::API
 
     def not_found
         render json: { error: 'not found' }
+    end
+
+    def authorize_request
+        header = request.headers['Authorization']
+        header = header.split(' ').last if header
+        begin
+            @decoded = JsonWebToken.decode(header)
+        rescue JWT::DecodeError => e
+            render json: { errors: e.message }, status: :unauthorized
+        end
+    end
 end
